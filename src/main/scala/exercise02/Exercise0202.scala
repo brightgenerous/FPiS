90ac4ec62ed9aecc7bb480b55186d812c7758074
brightgenerous <katou.akihiro@gmail.com>
2016-05-15 14:45:34 +0900

    add exercise 2.2

---
+++ src/main/scala/exercise02/Exercise0202.scala
@@ -0,0 +1,31 @@
+package exercise02
+
+import scala.annotation.tailrec
+
+object Exercise0202 {
+
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+
+    @tailrec
+    def isSorted_inner(head: A, tail: Array[A]): Boolean =
+      if (tail.isEmpty)
+        true
+      else if (ordered(head, tail.head))
+        isSorted_inner(tail.head, tail.tail)
+      else
+        false
+
+    isSorted_inner(as.head, as.tail)
+  }
+
+  def main(args: Array[String]): Unit = {
+    Seq(
+      (Array(1, 2, 3), (a: Int, b: Int) => a < b),
+      (Array(1, 2, 3), (a: Int, b: Int) => a > b),
+      (Array(1, 3, 2), (a: Int, b: Int) => a < b)
+    ).foreach { array_func =>
+      val (array, func) = array_func
+      println(isSorted(array, func))
+    }
+  }
+}
