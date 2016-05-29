b431a2e3c1d0b5ae6765ef86976f6f3503287284
brightgenerous <katou.akihiro@gmail.com>
2016-05-29 14:09:31 +0900

    add exercise 3.6

--- src/main/scala/datastructures/List.scala
+++ src/main/scala/datastructures/List.scala
@@ -21,6 +21,11 @@ object List {
     case Cons(x, xs) => x * product(xs)
   }
 
+  def head[A](l: List[A]): A = l match {
+    case Cons(head, tail) => head
+    case _ => sys.error("list is empty")
+  }
+
   def tail[A](l: List[A]): List[A] = l match {
     case Cons(head, tail) => tail
     case _ => sys.error("list is empty")
@@ -53,6 +58,19 @@ object List {
   def append[A](a1: List[A], a2: List[A]): List[A] = a1 match {
     case Nil => a2
     case Cons(head, tail) => Cons(head, append(tail, a2))
+  }
+
+  def init[A](l: List[A]): List[A] = {
+
+    def init_inner(lst: List[A]): List[A] = lst match {
+      case Cons(h, Nil) => Nil
+      case Cons(h, t) => Cons(h, init_inner(t))
+    }
+
+    l match {
+      case Nil => sys.error("list is empty")
+      case _ => init_inner(l)
+    }
   }
 
   def apply[A](as: A*): List[A] =
---
+++ src/main/scala/exercise03/Exercise0306.scala
@@ -0,0 +1,11 @@
+package exercise03
+
+import datastructures.List
+
+object Exercise0306 {
+
+  def exec = List.init(List(1,2,3,4,5))
+
+  def main(args: Array[String]): Unit =
+    println(exec)
+}
