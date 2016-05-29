cde04fbafa6abf645a5a8c6136617b4fafa1f46d
brightgenerous <katou.akihiro@gmail.com>
2016-05-29 14:48:40 +0900

    add exercise 3.7

---
+++ src/main/scala/exercise/exercise03/Exercise0307.scala
@@ -0,0 +1,12 @@
+package exercise.exercise03
+
+object Exercise0307 {
+
+  def exec = """
+    foldRightを使って実装されたproductは、0.0を検出した場合に、直ちに再帰を中止して0.0を返せるか
+    無理そう
+  """
+
+  def main(args: Array[String]): Unit =
+    println(exec)
+}
